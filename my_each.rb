9de4beb025246127ab6719b0d73b90603c71db31
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-26 21:21:04 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
+require "pry"
+def my_each (words)# put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    ary = []
+    while i < words.length
+      yield (words[i])
+      binding.pry
+      i += 1
+    end
+    ary
+  else
+    nil
+  end
+end
+
+my_each do |word|
+  binding.pry 
+  return word
 end
