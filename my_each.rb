797d8555d7820bcc352f140113219afd2c857c01
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-26 21:25:01 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,9 @@
 require "pry"
-def my_each (words)# put argument(s) here
-  # code here
-  if block_given?
-    i = 0
-    ary = []
-    while i < words.length
-      yield (words[i])
-      binding.pry
-      i += 1
-    end
-    ary
-  else
-    nil
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
-end
-
-my_each do |word|
-  binding.pry 
-  return word
+  collection
 end
